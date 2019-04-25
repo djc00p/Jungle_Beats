98d177d209d0273b83d075e7f2d34bff566f004b
Deonte Cooper <deonte.cooper12@gmail.com>
2019-04-25 17:08:46 -0600

    Updated append method to add a new node

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -1,14 +1,22 @@
 require './lib/node'
 
 class LinkedList
-  attr_reader :head
+  attr_accessor :head
 
   def initialize
     @head = head
   end
 
   def append(data)
-    @head = Node.new(data)
+    if @head.nil?
+      @head = Node.new(data)
+    else
+      current_node = @head
+      while current_node.next_node != nil
+        current_node = current_node.next_node
+      end
+      current_node.next_node = Node.new(data)
+    end
   end
 
   def count
