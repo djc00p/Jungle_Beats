88f0f1bdcce0c497c9439800715e2877a77f7147
Deonte Cooper <deonte.cooper12@gmail.com>
2019-04-25 16:19:18 -0600

    Added intial method for count

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -1,7 +1,19 @@
+require './lib/node'
+
 class LinkedList
   attr_reader :head
 
   def initialize
     @head = head
+  end
+
+  def append(data)
+    @head = Node.new(data)
+  end
+
+  def count
+    elements = []
+    elements << @head
+    elements.count
   end
 end
