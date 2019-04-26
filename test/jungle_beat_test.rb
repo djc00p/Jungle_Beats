ac4f83c67f2f75ed5714ef1b3055e13c2d053a10
Deonte Cooper <deonte.cooper12@gmail.com>
2019-04-26 11:46:53 -0600

    Added Test for play method

--- test/jungle_beat_test.rb
+++ test/jungle_beat_test.rb
@@ -8,7 +8,6 @@ require './lib/jungle_beat'
 class JungleBeatTest < MiniTest::Test
   def setup
     @jb = JungleBeat.new
-    @jb.list = LinkedList.new
   end
 
   def test_existence_of_jungle_beat
@@ -25,5 +24,22 @@ class JungleBeatTest < MiniTest::Test
 
     assert_equal "deep", @jb.list.head.data
     assert_equal "doo", @jb.list.head.next_node.data
+  end
+
+  def test_it_can_count
+    @jb.append("deep doo ditt")
+    @jb.append("woo hoo shu")
+
+    assert_equal "deep", @jb.list.head.data
+    assert_equal "doo", @jb.list.head.next_node.data
+    assert_equal 6, @jb.count
+  end
+
+  def test_it_can_play
+    @jb.append("deep doo ditt")
+    @jb.append("woo hoo shu")
+    @jb.list.prepend("dop")
+    @jb.list.insert(4, "woo")
+    @jb.play
   end
 end
--- test/linked_list_test.rb
+++ test/linked_list_test.rb
@@ -8,9 +8,7 @@ class LinkedListTest < MiniTest::Test
 
   def setup
     @list = LinkedList.new
-    # @node = Node.new("plop")
   end
-  # binding.pry
 
   def test_existence_of_linked_list
     assert_instance_of LinkedList, @list
