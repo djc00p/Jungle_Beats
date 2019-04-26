c96547f0e9f79b08999e8b7b90fc048dd0a2e87d
Deonte Cooper <deonte.cooper12@gmail.com>
2019-04-26 11:47:15 -0600

    Added play method

--- lib/jungle_beat.rb
+++ lib/jungle_beat.rb
@@ -4,15 +4,23 @@ class JungleBeat
   attr_accessor :list
 
   def initialize
-    @list = list
+    @list = LinkedList.new
   end
 
   def append(data)
-    @list = LinkedList.new
     list_data = data.split
     list_data.each do |data|
       @list.append(data)
     end
     @list
+  end
+
+  def count
+    @list.count
+  end
+
+  def play
+    beats = @list.to_string
+    `say -r 200 #{beats}`
   end
 end
