1372e8e02cd51691e3171ad099f650b3ab352513
Deonte Cooper <deonte.cooper12@gmail.com>
2019-04-26 10:32:34 -0600

    Added test for find and includes? methods

--- test/linked_list_test.rb
+++ test/linked_list_test.rb
@@ -55,7 +55,52 @@ class LinkedListTest < MiniTest::Test
   def test_if_list_return_to_string_all_data
     @list.append("plop")
     @list.append("deep")
+    @list.append("peep")
 
-    assert_equal "plop deep", @list.to_string
+    assert_equal "plop deep peep", @list.to_string
+  end
+
+  def test_it_can_prepend_an_element
+    @list.append("plop")
+    @list.append("deep")
+    @list.prepend("dop")
+
+    assert_equal "dop plop deep", @list.to_string
+    assert_equal 3, @list.count
+  end
+
+  def test_it_can_insert_an_element
+    @list.append("plop")
+    @list.append("deep")
+    @list.prepend("dop")
+    @list.insert(1, "woo")
+
+    assert_instance_of Node, @list.node_location(@list.head, 0)
+    assert_equal "dop woo plop deep", @list.to_string
+    assert_equal 4, @list.count
+  end
+
+  def test_it_can_find_an_element
+    @list.append("plop")
+    @list.append("deep")
+    @list.prepend("dop")
+    @list.insert(1, "woo")
+
+
+    assert_equal "woo", @list.find(1,1)
+    assert_equal "woo plop", @list.find(1,2)
+    assert_equal "plop deep", @list.find(2,2)
+    assert_equal "plop deep", @list.find(2,3)
+    assert_equal "woo plop deep", @list.find(1,3)
+  end
+
+  def test_includes_returns_true_when_element_is_present
+    @list.append("plop")
+    @list.append("deep")
+    @list.prepend("dop")
+    @list.insert(1, "woo")
+
+    assert_equal true, @list.includes?("woo")
+    assert_equal false, @list.includes?("poo")
   end
 end
